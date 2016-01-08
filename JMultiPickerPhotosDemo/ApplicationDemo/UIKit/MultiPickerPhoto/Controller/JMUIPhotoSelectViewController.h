3a848f10ff2fdd291dd6346c794b3be38dfbb090
HuangMin <huangmin@jpush.cn>
2016-01-08 11:02:52 +0800

    改变工程 目录结构

---
+++ JMultiPickerPhotosDemo/ApplicationDemo/UIKit/MultiPickerPhoto/Controller/JMUIPhotoSelectViewController.h
@@ -0,0 +1,32 @@
+//
+//  HMPhotoSelectViewController.h
+//  HMPhotoPickerDemo
+//
+//  Created by HuminiOS on 15/11/16.
+//  Copyright © 2015年 HuminiOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Photos/Photos.h>
+#import <AssetsLibrary/AssetsLibrary.h>
+
+@class JMUIPhotoModel;
+@class JMUIPhotoSelectViewController;
+
+@protocol JMUIPhotoPickerViewControllerDelegate <NSObject>
+
+- (void)JMUIPhotoPickerViewController:(JMUIPhotoSelectViewController *)PhotoPickerVC
+                 selectedPhotoArray:(NSArray *)selected_photo_array;
+
+@end
+
+@interface JMUIPhotoSelectViewController : UIViewController<UICollectionViewDataSource,UICollectionViewDelegate,UICollectionViewDelegateFlowLayout>
+@property (weak, nonatomic)id<JMUIPhotoPickerViewControllerDelegate> photoDelegate;
+@property (strong,nonatomic) PHFetchResult *allFetchResult;
+@property (strong, nonatomic)PHCollection *photoCollection;
+
+@property (strong, nonatomic)ALAssetsGroup *assetsGroup;
+
+- (void)didSelectStatusChange:(JMUIPhotoModel *)model;
+- (void)finshToSelectPhoto:(JMUIPhotoModel *)model;
+@end
